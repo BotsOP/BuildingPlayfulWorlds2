9851577322f969b1e93f97acb1fb8c98df8b358b
BotsOP <prutmaild@gmail.com>
2021-02-14 02:34:39 +0100

    Added random placed corridors and fixed dungeon generator

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -13,10 +13,11 @@ public class DungeonGenerator: MonoBehaviour
 
     [Header("Dungeon Settings")]
     public int amountRooms;
-    public int width = 100;
-    public int height = 100;
-    public int minRoomSize = 3;
-    public int maxRoomSize = 7;
+    public int width;
+    public int height;
+    public int minRoomSize;
+    public int maxRoomSize;
+    public int widthCorridor;
 
     private Dictionary<Vector2Int, Tile> dungeonDictionary = new Dictionary<Vector2Int, Tile>();
     private List<Room> roomList = new List<Room>();
@@ -28,8 +29,9 @@ public class DungeonGenerator: MonoBehaviour
 
     private void AllocateRooms()
     {
-        for (int i = 0; i < amountRooms; i++)
-        {
+        int amountOfTries = 0;
+        int amountOfMaxTries = 100;
+        while(amountRooms > 1 || amountOfTries > amountOfMaxTries) {
             Room room = new Room()
             {
                 position = new Vector2Int(Random.Range(0, width), Random.Range(0, height)),
@@ -39,11 +41,12 @@ public class DungeonGenerator: MonoBehaviour
             if (CheckIfRoomFitsInDungeon(room))
             {
                 AddRoomToDungeon(room);
+                amountRooms--;
             }
-            else
-            {
-                i--;
-            }
+
+            amountOfTries++;
+            if(amountOfTries > amountOfMaxTries)
+                break;
         }
     }
 
@@ -81,50 +84,44 @@ public class DungeonGenerator: MonoBehaviour
     private void AllocateCorridors()
     {
 
-        for (int i = 0; i < roomList.Count; i++)
+        for (int i = 1; i < roomList.Count; i++)
         {
-            //modulo
-            //10 % 3 = 1
-            //20 % 3 = 2
-            // 0 1 2 3 4 5 [6 7]
             Room startRoom = roomList[i];
-            Room otherRoom = roomList[(i + Random.Range(1, roomList.Count - 1)) % roomList.Count];
+            Room otherRoom = roomList[0];
+            
+            int extraX = Random.Range(minRoomSize/2, startRoom.size.x - (widthCorridor - 1));
+            int extraY = Random.Range(minRoomSize/2, startRoom.size.y - (widthCorridor - 1));
+            Debug.Log(extraX + "    " + extraY);
 
-            // -1, 0, 1
-            int dirX = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.x - startRoom.position.x));
-            for(int x = startRoom.position.x; x != otherRoom.position.x; x += dirX)
+            for(int j = 0; j <= widthCorridor; j++)
             {
-                Vector2Int pos = new Vector2Int(x, startRoom.position.y);
-                if (!dungeonDictionary.ContainsKey(pos))
+                int dirX = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.x - startRoom.position.x));
+                int dirY = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.y - startRoom.position.y));
+
+                Debug.Log(dirX + "    " + (startRoom.position.x + extraX) + "    " + (otherRoom.position.x + extraX));
+                for(int x = startRoom.position.x + extraX; x != otherRoom.position.x + extraX; x += dirX)
                 {
-                    dungeonDictionary.Add(pos, Tile.Floor);
+                    PlaceCorridor(new Vector2Int(x, startRoom.position.y + extraY + (j * dirY)));
+                    if(x < -10 || x > 150)
+                        break;
+                }
+    
+                Debug.Log(dirY + "    " + (startRoom.position.y + extraY) + "    " + (otherRoom.position.y + extraY));
+                for (int y = startRoom.position.y + extraY; y != otherRoom.position.y + extraY; y += dirY)
+                {
+                    PlaceCorridor(new Vector2Int(otherRoom.position.x + j + extraX, y));
+                    if(y < -10 || y > 150)
+                        break;
                 }
             }
-
-            int dirY = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.y - startRoom.position.y));
-            for (int y = startRoom.position.y; y != otherRoom.position.y; y += dirY)
-            {
-                Vector2Int pos = new Vector2Int(otherRoom.position.x, y);
-                if (!dungeonDictionary.ContainsKey(pos))
-                {
-                    dungeonDictionary.Add(pos, Tile.Floor);
-                }
-            }
-
         }
-
-
-
     }
 
-
-
-
-
-
-
-
-
+    private void PlaceCorridor(Vector2Int pos)
+    {
+        if (!dungeonDictionary.ContainsKey(pos))
+            dungeonDictionary.Add(pos, Tile.Floor);
+    }
 
     private void BuildDungeon()
     {
@@ -133,35 +130,51 @@ public class DungeonGenerator: MonoBehaviour
             GameObject floor = Instantiate(FloorPrefab, new Vector3Int(kv.Key.x, 0, kv.Key.y), Quaternion.identity);
             allSpawnedObjects.Add(floor);
 
-            SpawnWallsForTile(kv.Key);
+            //SpawnWallsForTile(kv.Key);
         }
     }
 
-    private void SpawnWallsForTile(Vector2Int position)
+    private void AddPrebuildRooms()
     {
-        for (int x = -1; x <= 1; x++)
+        int widthRoom = 20;
+        int heightRoom = 20;
+        Room room = new Room()
         {
-            for (int z = -1; z <= 1; z++)
-            {
-                if(Mathf.Abs(x) == Mathf.Abs(z)) { continue; }
-                Vector2Int gridPos = position + new Vector2Int(x, z);
-                if (!dungeonDictionary.ContainsKey(gridPos))
-                {
-                    //Spawn Wall
-                    Vector3 direction = new Vector3(gridPos.x, 0, gridPos.y) - new Vector3(position.x, 0, position.y);
-                    GameObject wall = Instantiate(WallPrefab, new Vector3(position.x, 0, position.y), Quaternion.LookRotation(direction));
-                    allSpawnedObjects.Add(wall);
-                }
-            }
-        }
+            position = new Vector2Int(width/2 - widthRoom/2, height/2 - heightRoom/2),
+            size = new Vector2Int(widthRoom,heightRoom)
+        };
+
+        AddRoomToDungeon(room);
     }
+
+    // private void SpawnWallsForTile(Vector2Int position)
+    // {
+    //     for (int x = -1; x <= 1; x++)
+    //     {
+    //         for (int z = -1; z <= 1; z++)
+    //         {
+    //             if(Mathf.Abs(x) == Mathf.Abs(z)) { continue; }
+    //             Vector2Int gridPos = position + new Vector2Int(x, z);
+    //             if (!dungeonDictionary.ContainsKey(gridPos))
+    //             {
+    //                 //Spawn Wall
+    //                 Vector3 direction = new Vector3(gridPos.x, 0, gridPos.y) - new Vector3(position.x, 0, position.y);
+    //                 GameObject wall = Instantiate(WallPrefab, new Vector3(position.x, 0, position.y), Quaternion.LookRotation(direction));
+    //                 allSpawnedObjects.Add(wall);
+    //             }
+    //         }
+    //     }
+    // }
 
 
     public void GenerateDungeon()
     {
+        AddPrebuildRooms();
         AllocateRooms();
         AllocateCorridors();
         BuildDungeon();
+
+        Debug.Log(roomList.Count);
     }
 
 }
